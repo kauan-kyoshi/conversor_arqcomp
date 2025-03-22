f4a294710398437a54322816128a418f0558d927
kauan-kyoshi <kauan.lima@sptech.school>
2025-03-22 01:52:34 -0300

    alteração de background, e add novas funcionalidades

--- Conversor/index/script.js
+++ Conversor/index/script.js
@@ -1,15 +1,33 @@
 function converter() {
-    // Obter o valor digitado no campo de entrada (com ID demo1)
-    var decimal = Number(document.getElementById('demo1').value);
+    
+    var decimal = Number(demo1.value);
 
-    // Verificar se o valor digitado é um número válido
+    
     if (isNaN(decimal) || decimal === "") {
         alert("Por favor, digite um número válido.");
         return;
     }
 
-    // Atualizar os resultados nas divs correspondentes
-    document.getElementById('demo2').innerHTML = decimal.toString(8); // Octal
-    document.getElementById('demo3').innerHTML = decimal.toString(16); // Hexadecimal
-    document.getElementById('demo4').innerHTML = decimal.toString(2);  // Binário
+    
+    demo2.innerHTML = decimal.toString(8); 
+    demo3.innerHTML = decimal.toString(16); 
+    demo4.innerHTML = decimal.toString(2); 
+}
+
+function sim() {
+    
+    document.getElementById('calc').style.visibility = 'visible';
+}
+
+function nao() {
+    alert(' Esse não é o caminho da luz <br> Bye ;-; ');
+            
+            // Tenta fechar a guia
+            try {
+                window.close();
+            } catch (e) {
+                // Fallback para navegadores mais antigos
+                window.open('', '_self', '');
+                window.close();
+            }
 }
