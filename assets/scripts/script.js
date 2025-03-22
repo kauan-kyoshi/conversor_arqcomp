3ef162c40dd66bd5ad9ae80a91d123dce2eb6cd1
kauan-kyoshi <kauan.lima@sptech.school>
2025-03-22 19:07:16 -0300

    adicionando a funcionalidade de soma para hex

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -27,6 +27,37 @@ function nao() {
             }
 }
 
-function base(){
+var global_numero_1 =0
+var global_numero_2 =0
 
+function soma(){
+    var dec_1 = 0;
+    var dec_2 = 0;
+    var res = 0;
+
+
+    global_numero_1 = Number(numero_1.value);
+    global_numero_2 = Number(numero_2.value);
+
+    if(global_numero_1 == '' || global_numero_2 == ''){
+        alert("Por favor, digite um número válido.");
+        
+    }else if (isNaN(global_numero_1) || isNaN(global_numero_2)){
+        alert("Por favor, digite um número válido.");
+    }else{
+        if(base.value == 'hex'){
+            dec_1 = parseInt(global_numero_1);
+            dec_2 = parseInt(global_numero_2);
+
+            res= dec_1 + dec_2;
+            resp.innerHTML= resp.toString(16)
+            document.getElementById('calc').style.visibility = 'visible';
+        }else if(base.value =='oct'){
+
+        }else if(base.value == 'bin'){
+            
+        }
+    }
+
+    
 }
