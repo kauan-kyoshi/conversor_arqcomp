00252bef36bb562e2490ada41786318f07be4286
kauan-kyoshi <kauan.lima@sptech.school>
2025-03-22 16:53:27 -0300

    v0.3 adicionado novas funcionalidades e melhorado alguns alinhamentos

--- Conversor/index/script.js
+++ Conversor/index/script.js
@@ -1,33 +1,32 @@
 function converter() {
     
     var decimal = Number(demo1.value);
-
     
     if (isNaN(decimal) || decimal === "") {
         alert("Por favor, digite um número válido.");
-        return;
+        
+    }else{
+        demo2.innerHTML = decimal.toString(8); 
+        demo3.innerHTML = decimal.toString(16); 
+        demo4.innerHTML = decimal.toString(2); 
     }
-
-    
-    demo2.innerHTML = decimal.toString(8); 
-    demo3.innerHTML = decimal.toString(16); 
-    demo4.innerHTML = decimal.toString(2); 
 }
 
 function sim() {
-    
+
     document.getElementById('calc').style.visibility = 'visible';
 }
 
 function nao() {
-    alert(' Esse não é o caminho da luz <br> Bye ;-; ');
-            
-            // Tenta fechar a guia
+    alert(`Esse não é o caminho da luz 
+        Bye ;-; `);
             try {
                 window.close();
-            } catch (e) {
-                // Fallback para navegadores mais antigos
-                window.open('', '_self', '');
-                window.close();
+            } catch (erro) {
+                console.error(`Erro: ${erro.message}`) 
             }
+}
+
+function base(){
+
 }
